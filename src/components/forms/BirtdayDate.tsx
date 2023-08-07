fa30432993bd38bcc4c5f8f4497e9e3ef7b5a63c
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-07 17:40:30 +0300

    feat(forms): add classes in BirthdayDate component

--- src/components/forms/BirtdayDate.tsx
+++ src/components/forms/BirtdayDate.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function BirtdayDate(): React.JSX.Element {
   return (
     <>
-      <div className="w-full sm:w-[47%] border-b-2 border-zinc-200 px-1 py-3">
-        <label htmlFor="birthday" className="mr-1 text-gray-400">
-          Date of birth* :
+      <div className="w-full sm:w-[48%] border-b-2 border-zinc-200 py-3">
+        <label htmlFor="birthday" className="text-gray-400 mr-1">
+          Date of birth*
         </label>
         <input type="date" name="birthday" defaultValue="2000-05-27" />
       </div>
