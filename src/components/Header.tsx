25331201fa213f5210a8ab268c8a2889f88f95df
Fault1err <f4ult1err@gmail.com>
2023-08-09 18:02:16 +0300

    feat: added app logo with link to main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
+import Img from '../images/logo4.jpg';
 
 function Header(): React.JSX.Element {
   return (
     <header className='container mx-auto'>
       <div className='flex justify-between'>
-      <h1 className="text-xl text-stone-400 font-bold">eCommerce Application</h1>
+      <Link to="/"><img className="m-3" src={Img} alt="logo" width='150'/></Link>
       <Navigation />
       </div>
     </header>
