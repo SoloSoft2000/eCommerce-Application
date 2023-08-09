065f6de8e272e7adefb3825712482e5d3fa09895
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-09 10:11:07 +0300

    feat: install react-hook-form

--- src/components/forms/Email.tsx
+++ src/components/forms/Email.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 function Email(): React.JSX.Element {
+  const { register } = useFormContext();
+
   return (
     <input
       className="w-full border-b-2 border-zinc-200 py-3 px-1"
       placeholder="Email*"
       type="email"
-      name="Email"
+      // name="Email"
+      {...register('Email', {
+        required: {
+          value: true,
+          message: 'required',
+        },
+      })}
     />
   );
 }
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
 import Title from '../../components/forms/Title';
 import SwitchPageLinks from '../../components/forms/SwitchPageLinks';
 import Email from '../../components/forms/Email';
@@ -6,18 +7,26 @@ import Password from '../../components/forms/Password';
 import SubmitFormButton from '../../components/forms/SubmitFormBtn';
 
 function LoginPage(): React.JSX.Element {
+  const methods = useForm();
+
+  const onSubmit = methods.handleSubmit(async (data) => {
+    console.log(data);
+  });
+
   return (
     <main className="flex justify-center py-16">
       <div className="text-black w-[32rem] max-w-full text-left">
         <Title />
         <SwitchPageLinks pageName="login" />
-        <form className="px-2 sm:px-0">
-          <div className="mb-12">
-            <Email />
-            <Password />
-          </div>
-          <SubmitFormButton value="Sign in" />
-        </form>
+        <FormProvider {...methods}>
+          <form onSubmit={onSubmit} noValidate className="px-2 sm:px-0">
+            <div className="mb-12">
+              <Email />
+              <Password />
+            </div>
+            <SubmitFormButton value="Sign in" />
+          </form>
+        </FormProvider>
       </div>
     </main>
   );
