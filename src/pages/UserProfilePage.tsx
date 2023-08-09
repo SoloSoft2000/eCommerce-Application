889613c0bc2e83c262b6f928f7988ea7572a8b1e
Fault1err <f4ult1err@gmail.com>
2023-08-09 16:06:27 +0300

    feat: added stub design of pages

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Img from '../images/img-02.png';
 
 function UserProfilePage(): React.JSX.Element {
   return <div className='container mx-auto'>
-    {<h3 className='text-xl font-bold pt-20 text-center'>User Profile</h3>}
+    <h3 className='text-xl font-bold pt-20 text-center'>User Profile</h3>
+    <img className='mx-auto w-1/2 m-8' src={Img} alt="main picture"/>
     </div>;
 }
 
