c08a0af50819830e3bf40b5bbf196a21f1b266fe
Fault1err <f4ult1err@gmail.com>
2023-08-10 12:32:54 +0300

    feat: added styles to homepage file

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,10 @@ import Main from '../components/Main';
 
 function HomePage(): React.JSX.Element {
   const customer = useSelector((state: RootState) => state.customer);
-  return <div>{<h3>Welcome back, {customer.firstName}</h3>}<Main /></div>;
+  return <div className='container mx-auto'>
+    {<h3 className='text-l font-bold pt-10 ml-20'>Welcome back, {customer.firstName}</h3>}
+    <Main />
+  </div>;
 }
 
 export default HomePage;
