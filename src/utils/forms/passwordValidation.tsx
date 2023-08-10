09012318c21374291b292632029f2f2f3449e280
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-10 15:45:06 +0300

    feat: add password validation

--- src/utils/forms/passwordValidation.tsx
+++ src/utils/forms/passwordValidation.tsx
@@ -1,17 +1,30 @@
+function isStrongPassword(password: string): boolean {
+  const checkPassword =
+    password.length < 8 &&
+    !/[A-Z]/.test(password) &&
+    !/[a-z]/.test(password) &&
+    !/[a-z]/.test(password) &&
+    !/\d/.test(password) &&
+    password !== password.trim();
+  if (checkPassword) return false;
+
+  return true;
+}
+
+const errorMassage =
+  'Password requires 8+ characters, an uppercase letter, a lowercase letter, a digit, and optionally a special character, with no leading/trailing spaces.';
+
 const passwordValidation = {
   required: {
     value: true,
     message: 'This field is requied',
   },
   minLength: {
-    value: 6,
-    message: 'Ok',
+    value: 8,
+    message: errorMassage,
   },
-  // pattern: {
-  //   value: /^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$/,
-  //   message:
-  //     'Password requires 8+ characters, an uppercase letter, a lowercase letter, a digit, and optionally a special character, with no leading/trailing spaces.',
-  // },
+  validate: (value: string): string | boolean =>
+    isStrongPassword(value) || errorMassage,
 };
 
 export default passwordValidation;
