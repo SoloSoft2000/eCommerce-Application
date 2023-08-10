346c129b4b7ee1415466854768df2f80d21b3a7f
Fault1err <f4ult1err@gmail.com>
2023-08-10 12:19:50 +0300

    fix: changed div tags to main for correct footer position

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -6,7 +6,7 @@ import Img4 from '../images/img-04.png';
 
 function CatalogPage(): React.JSX.Element {
   return (
-    <div className="container mx-auto">
+    <main className="container mx-auto">
       <h3 className="text-xl font-bold pt-20 text-center">Catalog</h3>
       <div className="flex flex-wrap">
         <img className="mx-auto w-2/5 m-8" src={Img1} alt="main picture" />
@@ -14,7 +14,7 @@ function CatalogPage(): React.JSX.Element {
         <img className="mx-auto w-2/5 m-8" src={Img3} alt="main picture" />
         <img className="mx-auto w-2/5 m-8" src={Img4} alt="main picture" />
       </div>
-    </div>
+    </main>
   );
 }
 
