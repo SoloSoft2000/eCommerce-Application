d15a37055f476355edc6ecdbcc6de8034b210140
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-13 14:19:38 +0300

    refactor: remove unused interfaces

--- src/interfaces/forms/form-props.ts
+++ src/interfaces/forms/form-props.ts
@@ -2,14 +2,4 @@ interface FormProps {
   [key: string]: string;
 }
 
-// interface ValidationFunction {
-//   required: { value: boolean; message: string };
-//   pattern?: { value: RegExp; message: string };
-//   validate?: (value: string) => string | boolean;
-// }
-
-// interface TextFields {
-//   [key: string]: ValidationFunction;
-// }
-
 export default FormProps;
