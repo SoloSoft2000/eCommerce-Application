78b7dc62f2ae822a641dea25ae3997236beedd13
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-14 15:27:08 +0300

    feat: add formating birthday date

--- src/sdk/utils/handleUserRegistrationData.ts
+++ src/sdk/utils/handleUserRegistrationData.ts
@@ -1,13 +1,24 @@
 import { CustomerDraft } from '@commercetools/platform-sdk';
 import UserRegistrate from '../../interfaces/forms/user-registration';
 
+function formatBirthdayDate(date: Date | null | undefined): string | undefined {
+  if (date) {
+    const year = date.getFullYear().toString();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
+  return undefined;
+}
+
 function handleUserData(data: UserRegistrate): CustomerDraft {
   const newUser: CustomerDraft = {
     email: data.email,
     password: data.password,
     firstName: data.firstName,
     lastName: data.lastName,
-    // dateOfBirth: data.birthday,
+    dateOfBirth: formatBirthdayDate(data.birthday),
     addresses: [
       {
         key: '1',
@@ -24,6 +35,10 @@ function handleUserData(data: UserRegistrate): CustomerDraft {
         postalCode: data.billPostcode || data.shipPostcode,
       },
     ],
+    shippingAddresses: [1, 2],
+    defaultShippingAddress: 1,
+    billingAddresses: [0, 2],
+    defaultBillingAddress: 1,
   };
   return newUser;
 }
