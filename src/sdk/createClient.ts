c8944dd5cf5d7d6c3dfbeed1c096dee4ea4d2ec7
Eugene Solomonyk <solosoft2000@mail.ru>
2023-08-14 12:59:37 +0300

    feat: token storage

--- src/sdk/createClient.ts
+++ src/sdk/createClient.ts
@@ -7,11 +7,14 @@ import {
   Client,
 } from '@commercetools/sdk-client-v2';
 import { region, projectKey, clientId, clientSecret, scopes } from './config';
+import TokenStorage from './TokenStorage';
 
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
   host: `https://api.${region}.commercetools.com`,
   fetch,
 };
+
+export const token = new TokenStorage();
 
 const createMiddlewareOptions = (user?: {
   username: string;
@@ -25,6 +28,7 @@ const createMiddlewareOptions = (user?: {
     ...(user ? { user } : {}),
   },
   scopes,
+  tokenCache: token,
   fetch,
 });
 
