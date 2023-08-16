d52c6206fd7c6951d5b9520d123a692bb11a2c6f
Fault1err <f4ult1err@gmail.com>
2023-08-16 13:38:14 +0300

    refactor: minor code style changes

--- src/_pages/NoPage.tsx
+++ src/_pages/NoPage.tsx
@@ -7,33 +7,18 @@ import NoPageClasses from '../helpers/enum/components/noPageClasses';
 function NoPage(): React.JSX.Element {
   return (
     <main className={NoPageClasses.MAIN}>
-      <h3 className={NoPageClasses.H3}>
-        This page is not found
-      </h3>
-      <img
-        className={NoPageClasses.IMG}
-        src={Img}
-        alt="main picture"
-      />
+      <h3 className={NoPageClasses.H3}>This page is not found</h3>
+      <img className={NoPageClasses.IMG} src={Img} alt="main picture" />
       <ButtonLink />
       <div className="flex flex-col">
         <p className="text-center m-4">or visit other pages:</p>
-        <Link
-          className={NoPageClasses.LINK}
-          to="/catalog"
-        >
+        <Link className={NoPageClasses.LINK} to="/catalog">
           Catalog Product
         </Link>
-        <Link
-          className={NoPageClasses.LINK}
-          to="/basket"
-        >
+        <Link className={NoPageClasses.LINK} to="/basket">
           Basket
         </Link>
-        <Link
-          className={NoPageClasses.LINK}
-          to="/about"
-        >
+        <Link className={NoPageClasses.LINK} to="/about">
           About Us
         </Link>
       </div>
