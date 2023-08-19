6b0a399ec5970b0b50037c135975084db712fc4a
Fault1err <f4ult1err@gmail.com>
2023-08-20 02:03:46 +0300

    feat: added pages links to main page

--- src/сomponents/Main.tsx
+++ src/сomponents/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Img from '../assets/images/img-01.png';
+import NavForMain from './NavForMain';
 
 function Main(): React.JSX.Element {
   return (
     <main className="container mx-auto">
       <h3 className="text-xl font-bold pt-20 text-center">Main page</h3>
+      <NavForMain />
       <img className="mx-auto w-3/4 m-8" src={Img} alt="main picture" />
     </main>
   );
