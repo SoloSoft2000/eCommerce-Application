f0b37809d2cbc9eb5814099b11e31ff2cb8cb133
Eugene Solomonyk <solosoft2000@mail.ru>
2023-08-19 15:15:44 +0300

    fix: change getproducts

--- src/utils/sdk/getProducts.ts
+++ src/utils/sdk/getProducts.ts
@@ -1,18 +1,18 @@
 import {
   createApiBuilderFromCtpClient,
-  Product,
+  ProductProjection,
 } from '@commercetools/platform-sdk';
 import { projectKey } from './config';
 import createClient from './createClient';
 
-async function getProducts(): Promise<Product[]> {
+async function getProducts(): Promise<ProductProjection[]> {
   const ctpClient = createClient();
 
   const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
     projectKey,
   });
 
-  const productQuery = apiRoot.products().get();
+  const productQuery = apiRoot.productProjections().get();
 
   const response = await productQuery.execute();
   return response.body.results;
