cc25ef53ec6df8b978db6599ba962dda03288a51
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-20 12:27:34 +0300

    fix: move dispatch to catalog from navigation

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../utils/reducers/store';
 import Filter from '../сomponents/catalog/Filter';
 import ProductList from '../сomponents/catalog/ProductList';
+import getProducts from '../utils/sdk/getProducts';
+import setDataElements from '../utils/sdk/utils/handleProductData';
+import { setProductsArray } from '../utils/reducers/productsListReducer';
 
 function CatalogPage(): React.JSX.Element {
   const productArray = useSelector((state: RootState) => state.products);
-  console.log(productArray.data);
+  const dispatch = useDispatch();
+
+  const fetchData = useCallback(async () => {
+    try {
+      const products = await getProducts();
+      const data = await setDataElements(products);
+      dispatch(setProductsArray(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (productArray.data.length < 1) {
+      fetchData();
+    }
+  }, [productArray, fetchData]);
 
   return (
     <main className="container mx-auto flex flex-wrap sm:flex-nowrap gap-4 justify-between items-start py-10">
