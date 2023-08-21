242e54270717936c517f0d90aa24289449a8597e
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-21 10:42:30 +0300

    feat: sort by abc

--- src/utils/sdk/getProducts.ts
+++ src/utils/sdk/getProducts.ts
@@ -5,26 +5,29 @@ import {
 import { projectKey } from './config';
 import createClient from './createClient';
 
-async function getProducts(): Promise<ProductProjection[]> {
+async function getProducts(sort?: string): Promise<ProductProjection[]> {
   const ctpClient = createClient();
   const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
     projectKey,
   });
 
-  const productQuery = apiRoot.productProjections().get();
+  // const productQuery = apiRoot.productProjections().get();
 
-  const productQuery2 = apiRoot.productProjections().get({
-    queryArgs: {
-      limit: 4,
-      offset: 4,
-      sort: 'name.en-Us asc',
-      // filter: 'categories.id=2bc99d45-3bf9-4c80-9a14-ebb97da4b8c5',
-    },
+  const queryArgs: Record<string, string | number> = {
+    limit: 4,
+  };
+
+  if (sort) {
+    queryArgs.sort = `name.en-Us ${sort}`;
+  }
+
+  const productQuery = apiRoot.productProjections().get({
+    queryArgs,
   });
 
   const response = await productQuery.execute();
-  const response2 = await productQuery2.execute();
-  console.log(response2.body);
+  // const response2 = await productQuery2.execute();
+  // console.log(response2.body);
   return response.body.results;
 }
 
--- src/сomponents/catalog/Filter.tsx
+++ src/сomponents/catalog/Filter.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import BrandFilter from './filter/BrandFilter';
 import PriceFilter from './filter/PriceFilter';
+import getProducts from '../../utils/sdk/getProducts';
+import setDataElements from '../../utils/sdk/utils/handleProductData';
+import { setProductsArray } from '../../utils/reducers/productsListReducer';
 
 function Filter(): React.JSX.Element {
+  const [sortBy, setSortBy] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const selectedSort = e.target.value;
+    setSortBy(selectedSort);
+  };
+
+  const fetchData = useCallback(async () => {
+    try {
+      const products = await getProducts(sortBy);
+      const data = setDataElements(products);
+      dispatch(setProductsArray(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }, [dispatch, sortBy]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="w-full sm:w-96 flex flex-col gap-8">
       <input
@@ -15,10 +41,14 @@ function Filter(): React.JSX.Element {
 
       <label className="block font-bold">
         Sort by alphabet:
-        <select className="w-full py-2 border-b-2 font-base border-zinc-200 font-normal text-orange-500">
+        <select
+          className="w-full py-2 border-b-2 font-base border-zinc-200 font-normal text-orange-500"
+          value={sortBy}
+          onChange={handleSortChange}
+        >
           <option value="">Sort: Random</option>
-          <option>Sort: A-Z</option>
-          <option>Sort: Z-A</option>
+          <option value="asc">Sort: A-Z</option>
+          <option value="desc">Sort: Z-A</option>
         </select>
       </label>
 
