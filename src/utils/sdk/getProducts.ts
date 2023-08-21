07cff332717f5aff15f83acc67b5bb1876ee330c
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-08-21 10:10:44 +0300

    feat: sort by name

--- src/utils/sdk/getProducts.ts
+++ src/utils/sdk/getProducts.ts
@@ -7,7 +7,6 @@ import createClient from './createClient';
 
 async function getProducts(): Promise<ProductProjection[]> {
   const ctpClient = createClient();
-
   const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
     projectKey,
   });
@@ -15,12 +14,17 @@ async function getProducts(): Promise<ProductProjection[]> {
   const productQuery = apiRoot.productProjections().get();
 
   const productQuery2 = apiRoot.productProjections().get({
-    queryArgs: { limit: 4 },
+    queryArgs: {
+      limit: 4,
+      offset: 4,
+      sort: 'name.en-Us asc',
+      // filter: 'categories.id=2bc99d45-3bf9-4c80-9a14-ebb97da4b8c5',
+    },
   });
 
   const response = await productQuery.execute();
   const response2 = await productQuery2.execute();
-  console.log(response2.body.results);
+  console.log(response2.body);
   return response.body.results;
 }
 
