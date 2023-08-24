a8ca0510a8da490c8c13af7fbe7c768685e4de7b
Fault1err <f4ult1err@gmail.com>
2023-08-24 15:15:40 +0300

    feat: added initial markup for detailed product page

--- src/pages/DetailedProductPage.tsx
+++ src/pages/DetailedProductPage.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import Img from '../assets/images/img-01.png';
 
 function ProductPage(): React.JSX.Element {
   return (
     <main className="container mx-auto">
-      <h3 className="text-xl font-bold pt-20 text-center">Detailed Product</h3>
-      <img className="mx-auto w-1/2 m-8" src={Img} alt="main picture" />
+      <div className='flex justify-center mt-16'>
+        <div className='border-2 w-1/3'>Picture will be here</div>
+        <div className='ml-8 w-1/3 border-2'>
+          <h3 className='text-2xl font-bold mb-11'>Product name</h3>
+          <p className='text-xl mb-11'>Product description</p>
+          <div className='flex mb-11'> 
+            <div className='text-amber-600 text-2xl'>Reg Price</div>
+            <div className='text-2xl text-red-600 ml-2'>Sale price</div>
+          </div>
+          <button className='w-1/2 mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer'>Add to Cart</button>
+        </div> 
+      </div>
+     
     </main>
   );
 }
