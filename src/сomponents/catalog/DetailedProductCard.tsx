aa8ca07a2c2ec714e4809113effa00be128070b6
Fault1err <f4ult1err@gmail.com>
2023-08-25 16:36:58 +0300

    feat: added styles for discount prices

--- src/сomponents/catalog/DetailedProductCard.tsx
+++ src/сomponents/catalog/DetailedProductCard.tsx
@@ -9,8 +9,12 @@ function DetailedProductCard(props: ProductCardProps): React.JSX.Element {
         <h3 className='text-2xl font-bold mb-11'>{props.title}</h3>
         <p className='text-xl mb-11'>{props.description}</p>
         <div className='flex mb-11'>
-            <div className='text-amber-600 text-2xl'>{props.price}</div>
-            <div className='text-2xl text-red-600 ml-2'>{props.discount}</div>
+        <div className={`text-2xl ${props.discount ? 'line-through text-amber-600' : 'text-amber-600'}`}>{props.price}</div>
+            {props.discount ? (
+                <div className='text-2xl text-red-600 ml-2'>{props.discount}</div>
+              ) : (
+                <div className='text-2xl text-red-600 ml-2'>&nbsp;</div>
+              )}
         </div>
         <button className='w-1/2 mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer'>Add to Cart</button>
     </div> 
