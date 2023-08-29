00fa57f769a50180c63d9bdffc334673b91640fb
Eugene Solomonyk <solosoft2000@mail.ru>
2023-08-29 16:32:38 +0300

    feat: add new address button and styles

--- src/pages/profile/UserAdresses.tsx
+++ src/pages/profile/UserAdresses.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../utils/reducers/store';
 import UserInfoStyles from '../../assets/styles/userinfo.module.scss';
+import FormStyles from '../../assets/styles/form.module.scss';
 
 function UserAdresses(): React.JSX.Element {
   const user: Customer = useSelector((state: RootState) => state.customer);
@@ -12,11 +13,12 @@ function UserAdresses(): React.JSX.Element {
     <div>
       <ul>
         {user.addresses.map((address, idx) => (
-          <li key={idx} className="border border-gray-200">
+          <li key={idx} className="border border-gray-200 mb-3 shadow-lg">
              <div className="p-4">
-                <div className="text-sm font-semibold text-gray-700">{address.country === 'US' ? 'USA' : 'Canada'}, {address.postalCode}, {address.city}</div>
-                <div className="text-sm text-gray-600 mt-1 mb-1">{address.streetName}</div>
-                <div className="flex justify-around items-center">
+                <div className="ml-5 text-sm font-semibold text-gray-700">{address.country === 'US' ? 'USA' : 'Canada'}, {address.postalCode}, {address.city}</div>
+                <div className="ml-6 text-sm text-gray-600 mt-1 mb-1">{address.streetName}</div>
+                <hr />
+                <div className="mt-2 flex justify-around items-center">
                   <div>
                     <div className="flex items-center text-xs text-gray-500">
                       <input type="checkbox" className="ml-2 mr-2 accent-black" checked={user.billingAddressIds?.includes(address.id as string)} readOnly />
@@ -38,11 +40,15 @@ function UserAdresses(): React.JSX.Element {
                     </div>
                   </div>
                 </div>
-                <button className={UserInfoStyles.addressBtn}>Edit</button>
+                <div className='flex justify-end mt-2'>
+                  <button className={UserInfoStyles.addressBtn}>Edit</button>
+                  <button className={UserInfoStyles.addressBtn}>Remove</button>
+                </div>
               </div>
           </li>
         ))}
       </ul>
+      <button className={FormStyles.submit_btn}>Add new address</button>
     </div>
   );
 }
