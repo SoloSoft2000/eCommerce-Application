cf3edfc443c126e73c8722eb0f272c38227157ea
Eugene Solomonik <solosoft2000@mail.ru>
2023-09-04 09:43:17 +0400

    fix: update  'Set as' only for not checked address

--- src/pages/profile/UserAdresses.tsx
+++ src/pages/profile/UserAdresses.tsx
@@ -117,7 +117,7 @@ function UserAdresses(): React.JSX.Element {
                       )
                     }
                   />
-                  Set as Default Billing
+                  {address.id !== user.defaultBillingAddressId && 'Set as'} Default Billing
                 </div>
               </div>
               <div className="text-right">
@@ -133,7 +133,7 @@ function UserAdresses(): React.JSX.Element {
                   />
                 </div>
                 <div className="flex justify-end items-center text-xs text-gray-500">
-                  Set as Default Shipping
+                  {address.id !== user.defaultShippingAddressId && 'Set as'} Default Shipping
                   <input
                     type="radio"
                     className="ml-2 mr-2 accent-black"
