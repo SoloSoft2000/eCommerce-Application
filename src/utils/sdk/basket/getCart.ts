8da2a192b90023ad8c5aa1ad6e09da773f1cbc74
Eugene Solomonik <solosoft2000@mail.ru>
2023-09-07 17:14:09 +0400

    feat: add updateQuantity, refactor other cart func

--- src/utils/sdk/basket/getCart.ts
+++ src/utils/sdk/basket/getCart.ts
@@ -4,25 +4,24 @@ import createApiRoot from '../createApiRoot';
 async function getCart(): Promise<Cart> {
   const apiRoot = createApiRoot();
 
+  let cart;
   const savedCustomer = localStorage.getItem('CT-Customer-SignIn');
   if (savedCustomer) {
     const customer: Customer = JSON.parse(savedCustomer);
-    const cart = apiRoot
-      .carts()
-      .withCustomerId({ customerId: customer.id })
-      .get();
-    const { body } = await cart.execute();
-    return body;
+    cart = apiRoot.carts().withCustomerId({ customerId: customer.id }).get();
   }
 
   const savedAnonymCard = sessionStorage.getItem('CT-Cart-AnonymID');
   if (savedAnonymCard) {
-    const cart = apiRoot.carts().withId({ ID: savedAnonymCard }).get();
-    const { body } = await cart.execute();
-    return body;
+    cart = apiRoot.carts().withId({ ID: savedAnonymCard }).get();
   }
 
-  throw new Error('Neither customer nor anonym cart found.');
+  if (!cart) {
+    throw new Error('No cart found');
+  }
+
+  const { body } = await cart.execute();
+  return body;
 }
 
 export default getCart;
