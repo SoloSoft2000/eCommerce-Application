0d6fb08124b18366a5e182b1b739d73202c0535e
Fault1err <f4ult1err@gmail.com>
2023-09-07 20:20:28 +0300

    feat: linter checks

--- src/сomponents/ButtonAddToCart.tsx
+++ src/сomponents/ButtonAddToCart.tsx
@@ -3,7 +3,6 @@ import updateQuantity from '../utils/sdk/basket/updateQuantity';
 import { BtnAddToCartProps } from '../helpers/interfaces/catalog/catalog-props';
 
 function ButtonAddToCart(props: BtnAddToCartProps): React.JSX.Element {
-
   const { id } = props;
 
   const bthClick = (): void => {
@@ -14,10 +13,11 @@ function ButtonAddToCart(props: BtnAddToCartProps): React.JSX.Element {
 
   return (
     <button
-    onClick={bthClick}
-    className="max-md:block w-1/2 max-md:w-9/12 max-md:text-xs max-md:mx-auto mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer"
-  >Add to Cart
-  </button>
+      onClick={bthClick}
+      className="max-md:block w-1/2 max-md:w-9/12 max-md:text-xs max-md:mx-auto mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer"
+    >
+      Add to Cart
+    </button>
   );
 }
 
