44852e9cbfff2341b5bc126d54c330440c9ee13e
Fault1err <f4ult1err@gmail.com>
2023-09-07 21:08:14 +0300

    feat: linter checks

--- src/сomponents/ButtonAddToCart.tsx
+++ src/сomponents/ButtonAddToCart.tsx
@@ -3,7 +3,7 @@ import updateQuantity from '../utils/sdk/basket/updateQuantity';
 import { BtnAddToCartProps } from '../helpers/interfaces/catalog/catalog-props';
 
 function ButtonAddToCart(props: BtnAddToCartProps): React.JSX.Element {
-  const { id, btnCatalogClasses} = props;
+  const { id, btnCatalogClasses } = props;
 
   const bthClick = (): void => {
     if (id) {
@@ -11,15 +11,12 @@ function ButtonAddToCart(props: BtnAddToCartProps): React.JSX.Element {
     }
   };
 
-  const btnStyles: string = btnCatalogClasses
-  ? btnCatalogClasses
-  : 'max-md:block w-1/2 max-md:w-9/12 max-md:text-xs max-md:mx-auto mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer';
+  const btnStyles: string =
+    btnCatalogClasses ||
+    'max-md:block w-1/2 max-md:w-9/12 max-md:text-xs max-md:mx-auto mb-11 text-center rounded bg-black p-3 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer';
 
   return (
-    <button
-      onClick={bthClick}
-      className={btnStyles}
-    >
+    <button onClick={bthClick} className={btnStyles}>
       Add to Cart
     </button>
   );
