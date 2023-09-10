d908967a86caa9bf22b2326813243c1983b19bf7
Fault1err <f4ult1err@gmail.com>
2023-09-10 22:47:47 +0300

    feat: markup of basket page and promo component

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import Img from '../assets/images/img-04.png';
 import getCart from '../utils/sdk/basket/getCart';
+import BasketItemCard from '../сomponents/basket/BasketItemCard';
+import PromoInputForm from '../сomponents/basket/PromoInputForm';
 
 function BasketPage(): React.JSX.Element {
-  getCart().then(console.log).catch(console.error);
+  async function getBasketCart() {
+    try {
+      const cart = await getCart();
+      console.log(cart);
+    } catch (error) {
+      console.error('The Shopping Cart is empty!', error);
+    }
+  }
+  
+  getBasketCart();
 
   return (
     <main className="container mx-auto">
-      <h3 className="text-xl font-bold pt-20 text-center">Basket</h3>
-      <img className="mx-auto w-1/2 m-8" src={Img} alt="main picture" />
+      <h2 className="text-2xl font-bold pt-12 text-center">Shopping Cart</h2>
+      <div className='flex mt-8'>
+        <div className='border h-[70%] w-1/2'>
+          <BasketItemCard />
+          <BasketItemCard />
+          <BasketItemCard />
+        </div>
+        <div className='border h-[30%] w-1/2 ml-10'>
+          <h4 className="font-bold text-center text-xl mb-8 mt-3">Cart Totals</h4>
+          <PromoInputForm />
+          <div className="flex max-md:flex-col justify-center">
+            <div className='flex w-full ml-[5%] mb-2'>
+              <p className="max-lg:text-sm text-xl text-slate-800 font-bold pr-1">
+                Total:
+              </p>
+              <p className="max-lg:text-sm text-xl font-bold px-1 max-md:px-0">
+                100 $
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <img className="mx-auto w-1/2 m-8" src={Img} alt="main picture" /> */}
     </main>
   );
 }
---
+++ src/сomponents/basket/BasketItemCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BasketItemCard(): React.JSX.Element {
+    
+    return (
+    <div className="m-1 border-[1px] border-slate-200 w-[18rem] h-[30rem] h-auto bg-white rounded relative cursor-pointer mb-8">
+      Product
+    </div>
+  );
+}
+
+export default BasketItemCard;
+
