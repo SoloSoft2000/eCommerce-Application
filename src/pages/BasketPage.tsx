97cc20baddc8075f225f78fed0757870d9985ce3
Fault1err <f4ult1err@gmail.com>
2023-09-10 23:41:51 +0300

    feat: added initial markup of basket item card

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Img from '../assets/images/img-04.png';
 import getCart from '../utils/sdk/basket/getCart';
 import BasketItemCard from '../сomponents/basket/BasketItemCard';
 import PromoInputForm from '../сomponents/basket/PromoInputForm';
@@ -19,13 +18,16 @@ function BasketPage(): React.JSX.Element {
   return (
     <main className="container mx-auto">
       <h2 className="text-2xl font-bold pt-12 text-center">Shopping Cart</h2>
-      <div className='flex mt-8'>
-        <div className='border h-[70%] w-1/2'>
+      <div className='flex mt-8 max-lg:flex-col'>
+        <div className='border h-[70%] w-1/2 max-lg:w-full max-lg:m-2'>
+          <BasketItemCard />
+          <BasketItemCard />
+          <BasketItemCard />
           <BasketItemCard />
           <BasketItemCard />
           <BasketItemCard />
         </div>
-        <div className='border h-[30%] w-1/2 ml-10'>
+        <div className='border h-[30%] w-1/2 ml-10 max-lg:w-full max-lg:ml-0 max-lg:mt-2 max-lg:m-2'>
           <h4 className="font-bold text-center text-xl mb-8 mt-3">Cart Totals</h4>
           <PromoInputForm />
           <div className="flex max-md:flex-col justify-center">
@@ -40,7 +42,6 @@ function BasketPage(): React.JSX.Element {
           </div>
         </div>
       </div>
-      {/* <img className="mx-auto w-1/2 m-8" src={Img} alt="main picture" /> */}
     </main>
   );
 }
