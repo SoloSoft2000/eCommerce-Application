6852d772fca0a25dfb80f79e2bd25f433da4d55a
Fault1err <f4ult1err@gmail.com>
2023-09-11 10:57:56 +0300

    feat: added mock remove from cart button

--- src/сomponents/basket/BasketItemCard.tsx
+++ src/сomponents/basket/BasketItemCard.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import ButtonRemoveFromCart from '../ButtonRemoveFromCart';
+// import { ProductCardProps } from '../../helpers/interfaces/catalog/catalog-props';
+import SubmitFormButton from '../forms/SubmitFormBtn';
 
 function BasketItemCard(): React.JSX.Element {
-
+   
     return (
         <div className="m-1 max-md:mt-8 border rounded mb-8 flex justify-betweeen">
             <div className="h-[130px] w-[130px] border">
@@ -34,7 +37,18 @@ function BasketItemCard(): React.JSX.Element {
                 Price
            </div>
         </div>
-        <div className='border w-1/4 ml-5'>Quantity Block</div>
+        <div className='border w-1/4 ml-5'>
+            <div>Spinner</div>
+            <div>
+                {/* <ButtonRemoveFromCart setUpdateFlag={setUpdateFlag} id={props.id} btnCartClasses={true}/> */}
+                <div className='ml-[34%]'>
+                    <SubmitFormButton 
+                    value="Remove"
+                    classes='w-1/2 max-md:w-1/2 text-xs text-center rounded bg-black p-2 text-white uppercase drop-shadow-sm hover:bg-slate-600 cursor-pointer;'
+                    />
+                </div>                
+            </div>
+        </div>
     </div>
     );
 }
