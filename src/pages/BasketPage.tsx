c26a33bf2ddfe369c9c46572a8fa47f21ba57ddf
Fault1err <f4ult1err@gmail.com>
2023-09-11 13:03:09 +0300

    feat: added empty cart message with catalog link

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import getCart from '../utils/sdk/basket/getCart';
 import BasketItemCard from '../сomponents/basket/BasketItemCard';
 import PromoInputForm from '../сomponents/basket/PromoInputForm';
+import { Cart } from '@commercetools/platform-sdk';
+import ToCatalogLink from '../сomponents/basket/ToCatalogLink';
 
 function BasketPage(): React.JSX.Element {
-  async function getBasketCart() {
-    try {
-      const cart = await getCart();
-      console.log(cart);
-    } catch (error) {
-      console.error('The Shopping Cart is empty!', error);
+  const [cart, setCart] = useState<Cart | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getBasketCart(): Promise<void> {
+      try {
+        const cart: Cart = await getCart();
+        setCart(cart);
+        console.log(cart);
+      } catch (error) {
+        console.error('Error fetching cart:', error);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  }
-  
-  getBasketCart();
+    getBasketCart();
+  }, [cart]);
 
   return (
     <main className="container mx-auto">
       <h2 className="text-2xl font-bold pt-12 text-center">Shopping Cart</h2>
       <div className='flex mt-8 max-lg:flex-col'>
-        <div className='border h-[70%] w-1/2 max-lg:w-full max-lg:m-2'>
-          <BasketItemCard />
-          <BasketItemCard />
-          <BasketItemCard />
-          <BasketItemCard />
-          <BasketItemCard />
-          <BasketItemCard />
+      <div className='border min-h-[70%] w-1/2 max-lg:w-full max-lg:m-2'>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : cart ? (
+            <>
+              <BasketItemCard />
+              <BasketItemCard />
+              <BasketItemCard />
+              <BasketItemCard />
+              <BasketItemCard />
+              <BasketItemCard />
+            </>
+          ) : (
+            <>
+              <p className='text-2xl font-bold pt-12 text-center'>The Shopping Cart is empty</p>
+              <p className='text-xl pt-10 text-center mb-8'>Let's start shopping and ...</p>
+              <ToCatalogLink />
+            </>
+          )}
         </div>
         <div className='border h-[30%] w-1/2 ml-10 max-lg:w-full max-lg:ml-0 max-lg:mt-2 max-lg:m-2'>
           <h4 className="font-bold text-center text-xl mb-8 mt-3">Cart Totals</h4>
