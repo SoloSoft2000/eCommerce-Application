0ed0af034af79982aa65faf6002ac5450daea316
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-09-13 16:06:24 +0300

    feat: promocode image

--- src/сomponents/Main.tsx
+++ src/сomponents/Main.tsx
@@ -4,11 +4,12 @@ import Img1 from '../assets/images/img-01.png';
 import Img2 from '../assets/images/img-02.png';
 import Img3 from '../assets/images/img-03.png';
 import Img4 from '../assets/images/img-04.png';
+import Promo1 from '../assets/images/promoFirst.jpg';
 
 function Main(): React.JSX.Element {
   return (
     <main className="container mx-auto">
-      <div className="py-10 md:py-20 text-center font-bold">
+      <div className="py-10 md:py-16 text-center font-bold">
         <h1 className="text-2xl text-amber-600 mb-6">
           Sparkle and Shine: Unforgettable Jewelry Designs
         </h1>
@@ -16,21 +17,42 @@ function Main(): React.JSX.Element {
           Discover a world of possibilities in our store
         </h2>
       </div>
-
-      <div className="flex justify-around items-center flex-wrap gap-8 md:gap-y-14 py-8">
-        <div className="w-[45%]">
-          <MainCategoryCover img={Img3} name={'All products'} />
+      <section>
+        <h2 className="text-xl font-bold text-slate-700">
+          Deals and Discounts
+        </h2>
+        <div className="flex justify-around items-center flex-wrap gap-8 md:gap-y-14 py-8">
+          <img
+            className="flex justify-center items-center mx-auto w-full md:w-[45%]
+          rounded-2xl"
+            src={Promo1}
+          ></img>
+          <img
+            className="flex justify-center items-center mx-auto w-full md:w-[45%]
+          rounded-2xl"
+            src={Promo1}
+          ></img>
         </div>
-        <div className="w-[45%]">
-          <MainCategoryCover img={Img1} name={'Rings'} />
+      </section>
+      <section>
+        <h2 className="text-xl font-bold text-slate-700	">
+          Product Collections
+        </h2>
+        <div className="flex justify-around items-center flex-wrap gap-8 md:gap-y-14 py-8">
+          <div className="w-full md:w-[45%]">
+            <MainCategoryCover img={Img3} name={'All products'} />
+          </div>
+          <div className="w-full md:w-[45%]">
+            <MainCategoryCover img={Img1} name={'Rings'} />
+          </div>
+          <div className="w-full md:w-[45%]">
+            <MainCategoryCover img={Img2} name={'Earrings'} />
+          </div>
+          <div className="w-full md:w-[45%]">
+            <MainCategoryCover img={Img4} name={'Necklaces'} />
+          </div>
         </div>
-        <div className="w-[45%]">
-          <MainCategoryCover img={Img2} name={'Earrings'} />
-        </div>
-        <div className="w-[45%]">
-          <MainCategoryCover img={Img4} name={'Necklaces'} />
-        </div>
-      </div>
+      </section>
     </main>
   );
 }
