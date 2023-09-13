edbb21a63d28b24c60e9dfb9f0c5458e24205801
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-09-13 14:46:07 +0300

    fix: test

--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,26 +1,27 @@
 import App from '../src/App';
 
 jest.mock('validator/es/lib/isEmail', () => ({
-    __esModule: true,
-    default: jest.fn(),
+  __esModule: true,
+  default: jest.fn(),
 }));
 
 jest.mock('swiper/react', () => ({
-    Swiper: ({ children }: { children: React.ReactNode }) => children,
-    SwiperSlide: ({ children }: { children: React.ReactNode }) => children,
+  Swiper: ({ children }: { children: React.ReactNode }): React.ReactNode =>
+    children,
+  SwiperSlide: ({ children }: { children: React.ReactNode }): React.ReactNode =>
+    children,
 }));
-  
-  jest.mock('swiper/modules', () => ({
-    Navigation: (props: string) => null,
-    Pagination: (props: string) => null,
+
+jest.mock('swiper/modules', () => ({
+  Navigation: (props: string) => null,
+  Pagination: (props: string) => null,
 }));
-  
-jest.mock('swiper/css', () => jest.fn());
-jest.mock('swiper/css/navigation', () => jest.fn());
-jest.mock('swiper/css/pagination', () => jest.fn());
-  
+
+jest.mock('swiper/css', () => 'swiper/css');
+jest.mock('swiper/css/navigation', () => 'swiper/css/navigation');
+jest.mock('swiper/css/pagination', () => 'swiper/css/pagination');
 
 test('App is defined and it is a function', () => {
-    expect(App).toBeDefined();
-    expect(typeof App).toBe('function');    
+  expect(App).toBeDefined();
+  expect(typeof App).toBe('function');
 });
