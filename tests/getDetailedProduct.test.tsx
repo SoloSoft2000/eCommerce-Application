2bdf39d1d148e132f5a1ae909f9b076b1cf82b67
Fault1err <f4ult1err@gmail.com>
2023-09-17 19:18:32 +0300

    refactor: updated jest tests

--- tests/getDetailedProduct.test.tsx
+++ tests/getDetailedProduct.test.tsx
@@ -1,23 +1,36 @@
 import returnProductById from '../src/utils/sdk/getDetailedProduct';
 
-jest.mock('../src/utils/sdk/createApiRoot', () => {
-  return () => ({
-    productProjections: () => ({
-      withId: jest.fn(() => ({
-        get: jest.fn(() => ({
-          execute: jest.fn(() => ({
-            body: {  id: '123' }
-          })),
+type MockReturnValue = {
+  productProjections: () => {
+    withId: () => {
+      get: () => {
+        execute: () => {
+          body: {
+            id: string;
+          };
+        };
+      };
+    };
+  };
+};
+
+jest.mock('../src/utils/sdk/createApiRoot', () => (): MockReturnValue => ({
+  productProjections: () => ({
+    withId: jest.fn(() => ({
+      get: jest.fn(() => ({
+        execute: jest.fn(() => ({
+          body: { id: '123' }
         })),
       })),
-    }),
-  });
-});
+    })),
+  }),
+}));
+
 
 describe('returnProductById', () => {
-  it('should return a product when the product ID provides', async () => {
+  it('should return a product when the product ID is provided', async () => {
     const productId = '123'; 
-    const result = await returnProductById(productId);
+    const result: { id: string } = await returnProductById(productId);
     expect(result.id).toBe(productId); 
     expect(result).toBeDefined(); 
   });
