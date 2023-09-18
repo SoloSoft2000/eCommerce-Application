d5bb4900b7602fa8029f5ec0a77d00e6e97e9cf8
Eugene Solomonyk <solosoft2000@mail.ru>
2023-09-18 07:46:30 +0300

    fix: update basket count after login logout

--- src/utils/sdk/getCustomers.ts
+++ src/utils/sdk/getCustomers.ts
@@ -1,5 +1,8 @@
 import { Customer /* , MyCustomerSignin */ } from '@commercetools/platform-sdk';
 import createApiRoot from './createApiRoot';
+import getCart from './basket/getCart';
+import store from '../reducers/store';
+import { setCartCount } from '../reducers/cartCountReducer';
 
 async function getCustomers(
   username: string,
@@ -22,6 +25,12 @@ async function getCustomers(
   await customerExec.execute();
   const ap = createApiRoot('password', username, password);
   const { body } = await ap.me().get().execute();
+
+  getCart()
+    .then((dataCart) =>
+      store.dispatch(setCartCount(dataCart.totalLineItemQuantity))
+    )
+    .catch(() => store.dispatch(setCartCount(0)));
   return body;
 }
 
